c128aa7cff4aef04d6871c703bb41c88f39d5536
Adrian Franzese <adrianfranzese@gmail.com>
2018-05-27 20:14:16 +1000

    Removed old code, renamed file names

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 let scripts, sourceAdrian, sourceTrump
 let rmAdrian, rmTrump
 let scene, adrianHTML, trumpHTML, adrianProfile, trumpProfile;
-let adrianText, trumpText, adrianTokens, trumpTokens
+let adrianText, trumpText
 let order = 3;
 
 function preload() {
@@ -13,20 +13,11 @@ function preload() {
 
 function setup() {
   noCanvas();
-  // createCanvas(windowWidth,windowHeight).id('canvas')
-  // createP("Click to start").parent('scene');
-
   // Initialize RiTa Markov with source text
   rmAdrian = new RiMarkov(order);
   rmAdrian.loadText(sourceAdrian.join(' '));
   rmTrump = new RiMarkov(order);
   rmTrump.loadText(sourceTrump.join(' '));
-
-  // scene = createP('').parent('#scene')
-  // createElement('b', 'ADRIAN').parent('#dialogue').class('name');
-  // adrianHTML = createP('...').parent("dialogue")
-  // createElement('b', 'TRUMP').parent('#dialogue').class('name');
-  // trumpHTML = createP('...').parent("dialogue")
 
   // Initialize HTML elements
   scene = select('p', '#scene')
@@ -36,54 +27,28 @@ function setup() {
   adrianProfile = select('#adrianProfile')
   trumpProfile = select('#trumpProfile')
 
-  // setInterval(mousePressed, 10000);
-
+  // Creates a new scene every 10 seconds (for display purposes)
+  setInterval(newScene, 10000);
 }
 
-function mousePressed() {
+function newScene() {
+  // Clear array of image urls
   imageTags = []
-  //scene.remove();
-  //dialogue.remove();
-
-  // select('#scene').remove();
-  // select('#dialogue').remove();
-  // select('#trumpProfile').remove();
-  // select('#adrianProfile').remove();
-  // clear();
-
-  // createDiv('').id('scene').parent('script');
-  // createP( random(scripts) ).parent('scene');
-
-  //dialogue = createDiv('').parent(container);
+  // Generate a new sentence from sources
   adrianText = rmAdrian.generateSentence()
   trumpText = rmTrump.generateSentence();
 
-  // adrianTokens = splitTokens(adrianText, [' ', '.', '?', '!', '/'])
-  // trumpTokens = splitTokens(trumpText, [' ', '.', '?', '!', '/'])
-  adrianTokens = split(adrianText, ' ')
-  trumpTokens = split(trumpText, ' ')
-  // console.log(adrianTokens)
-  // console.log(trumpTokens)
-
+  // Print a random scene from scripts file
   scene.html(random(scripts))
+  // Print the newly generated sentences
   adrianHTML.html(adrianText)
   trumpHTML.html(trumpText)
 
+  // Run Azure sentiment analysis on the generated texts
   getAzure(sentiment, trumpText, adrianText);
+  // Run Azure keyword analysis on the generated texts
   getAzure(keywords, trumpText, adrianText);
-
-  // createDiv('').id('dialogue').parent('script');
-  // createP( adrianText ).parent('dialogue');
-  //
-  // createP( trumpText ).parent('dialogue');
-
-
-  //console.log( s.analyze(trumpText).score );
-
-  //createImg('data/trump_pos.jpg').id('trumpProfile');
-
-
-
+  // Prepare to enter ~ ~ CALLBACK HELL ~ ~
 }
 
 // Fullscreen on/off on "Spacebar" press
@@ -94,6 +59,7 @@ function keyReleased() {
   }
 }
 
+// Endpoint APIs for Azure
 let sentiment = 'https://australiaeast.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment';
 let keywords = 'https://australiaeast.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases';
 
@@ -116,7 +82,7 @@ function getAzure(url, sentence01, sentence02) {
     url, {
       method: 'POST',
       headers: {
-        'Ocp-Apim-Subscription-Key': '6d720b9eb27e47b595cae27a1ee85461',
+        'Ocp-Apim-Subscription-Key': '6d720b9eb27e47b595cae27a1ee85461', // Shhhh
         'Content-Type': 'application/json'
       },
       body: payload
@@ -137,33 +103,20 @@ function getAzure(url, sentence01, sentence02) {
 
 function runSentimentCode(results) {
   let trumpMood = results.documents[0].score;
-  // console.log(results.documents[0].score);
   if (trumpMood > 0.6) {
-    // console.log('Trump is feeling good');
-    // createImg('data/images/trump_good.jpg').id('trumpProfile').parent('#container');
     trumpProfile.style('background-image', 'url("data/images/trump_good.gif")')
   } else if (trumpMood < 0.4) {
-    // console.log('Trump is feeling bad')
-    // createImg('data/images/trump_bad.jpg').id('trumpProfile').parent('#container');
     trumpProfile.style('background-image', 'url("data/images/trump_bad.gif")')
   } else {
-    // console.log('Trump is neutral')
-    // createImg('data/images/trump_neutral.jpg').id('trumpProfile').parent('#container');
     trumpProfile.style('background-image', 'url("data/images/trump_neutral.gif")')
   }
 
   let adrianMood = results.documents[1].score;
   if (adrianMood > 0.6) {
-    // console.log('Adrian is feeling good');
-    // createImg('data/images/trump_good.jpg').id('adrianProfile').parent('#container');
     adrianProfile.style('background-image', 'url("data/images/adrian_good.gif")')
   } else if (adrianMood < 0.4) {
-    // console.log('Adrian is feeling bad');
-    // createImg('data/images/trump_good.jpg').id('adrianProfile').parent('#container');
     adrianProfile.style('background-image', 'url("data/images/adrian_bad.gif")')
   } else {
-    // console.log('Adrian is neutral');
-    // createImg('data/images/trump_good.jpg').id('adrianProfile').parent('#container');
     adrianProfile.style('background-image', 'url("data/images/adrian_neutral.gif")')
   }
 }
@@ -191,21 +144,15 @@ function searchImages(who, keyword) {
     imageSearchURL + escaped, {
       method: 'GET',
       headers: {
-        'Ocp-Apim-Subscription-Key': '4f9d3e47c3234199a718f4bfa8180e43'
+        'Ocp-Apim-Subscription-Key': '4f9d3e47c3234199a718f4bfa8180e43' // Shhhh
       }
     },
     function(res) {
       res = JSON.parse(res)
       if (res.value[0]) {
-        // let imgURL = res.value[0].thumbnailUrl
-        // img = createImg(res.value[0].thumbnailUrl, keyword).class('inlineImage')
         img = '<img src=\"' + res.value[0].thumbnailUrl + '\" class=\"inlineImage\">'
-        // imageTags.push([who, keyword, img.elt.outerHTML])
         imageTags.push([who, keyword, img])
         insertImages()
-        //  [0]  [1]      [2]
-        // let i = trumpText.match(keyword).index
-        // trumpHTML.html( trumpHTML.html().substring(0, i) + img.elt.outerHTML + ' ' + trumpHTML.html().substring(i) )
       }
     },
     function(error) {
@@ -223,15 +170,12 @@ function insertImages() {
     let imgElem = item[2]
     if (meOrTrump == 0) {
       let splitPoint = trumpReplacedText.match(keyword).index
-      // trumpHTML.html( trumpHTML.html().substring(0, i) + x[1] + ' ' + trumpHTML.html().substring(i) )
       trumpReplacedText = trumpReplacedText.substring(0, splitPoint) + imgElem + ' ' + trumpReplacedText.substring(splitPoint)
     } else if (meOrTrump == 1) {
       let splitPoint = adrianReplacedText.match(keyword).index
-      // adrianHTML.html( adrianHTML.html().substring(0, i) + x[1] + ' ' + adrianHTML.html().substring(i) )
       adrianReplacedText = adrianReplacedText.substring(0, splitPoint) + imgElem + ' ' + adrianReplacedText.substring(splitPoint)
     }
   }
-  console.log(trumpReplacedText)
   trumpHTML.html(trumpReplacedText)
   adrianHTML.html(adrianReplacedText)
 }
